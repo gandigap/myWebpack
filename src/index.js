1127a9c54f5cd8696f11ca5ac51adfd8faed03a7
Ihar Berasneu <igor.simatic@gmail.com>
2021-10-11 14:01:04 +0300

    add babel config

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ const exampleData = {
   typeInfo: 'example',
 }
 
+const exampleData2 = {
+  ...exampleData,
+  typeClass: 'string',
+  typeMode: true,
+}
+
+
 console.log(exampleData);
+console.log(exampleData2);
